598a175dfbd83d9b3527ae25868b0c2cb24ca802
Andyspfl <andymaytac@gmail.com>
2024-05-19 19:19:21 -0500

    Avance 15/05/2023

--- Semana-9/mvc-api-books/app/controllers/book_controller.py
+++ Semana-9/mvc-api-books/app/controllers/book_controller.py
@@ -1,17 +1,32 @@
 from flask import Blueprint, request, jsonify
 from models.book_model import Book
 from views.book_view import render_book_detail, render_book_list
+from flask_jwt_extended import verify_jwt_in_request, get_jwt_identity
+from functools import wraps
 
 # Crear un blueprint para el controlador de animales
 book_bp = Blueprint("book",__name__)
 
+def jwt_required(fn):
+    @wraps(fn)
+    def wrapper(*args, **kwargs):
+        try:
+            verify_jwt_in_request()
+            return fn(*args, **kwargs)
+        except Exception as e:
+            return jsonify({"error": str(e)}), 401
+
+    return wrapper
+
 # Ruta para obtener la lista de animales
 @book_bp.route("/books", methods=["GET"])
+@jwt_required
 def get_books():
     books=Book.get_all()
     return jsonify(render_book_list(books))
 
 @book_bp.route("/books/<int:id>", methods=["GET"])
+@jwt_required
 def get_book(id):
     book = Book.get_by_id(id)
     if book:
@@ -20,6 +35,7 @@ def get_book(id):
         
 
 @book_bp.route("/books", methods=["POST"])
+@jwt_required
 def create_book():
     data=request.json
     title= data.get("title")
@@ -35,6 +51,7 @@ def create_book():
     return jsonify(render_book_detail(book)), 201
     
 @book_bp.route("/books/<int:id>", methods=["PUT"])
+@jwt_required
 def update_book(id):
     book = Book.get_by_id(id)
     if not book:
@@ -50,6 +67,7 @@ def update_book(id):
     return jsonify(render_book_detail(book))
 
 @book_bp.route("/books/<int:id>", methods=["DELETE"])
+@jwt_required
 def delete_book(id):
     book = Book.get_by_id(id)
     if not book:
